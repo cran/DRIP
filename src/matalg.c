c50345577fe1fa30d78777b861f1001d7d6a32c8
Yicheng Kang <kangx276@umn.edu>
2022-04-06 20:52:28 +0000

    version 1.7

--- src/matalg.c
+++ src/matalg.c
@@ -1,15 +1,21 @@
+#define USE_FC_LEN_T
+
 #include <R.h>
 #include <math.h>
 #include <Rmath.h>
 #include <R_ext/Lapack.h>
 
+#ifndef FCONE
+# define FCONE
+#endif
+
 void matsolve(double *a, double *b, int *nrowb, int *ncolb)
 {
     int info;
-    F77_CALL(dpotrf)("L", nrowb, a, nrowb, &info);
+    F77_CALL(dpotrf)("L", nrowb, a, nrowb, &info FCONE);
     if (info != 0)
         error("Cholesky decomposition failed");
-    F77_CALL(dpotrs)("L", nrowb, ncolb, a, nrowb, b, nrowb, &info);
+    F77_CALL(dpotrs)("L", nrowb, ncolb, a, nrowb, b, nrowb, &info FCONE);
     if (info != 0)
         error("solution failed");
 }
@@ -17,7 +23,7 @@ void matsolve(double *a, double *b, int *nrowb, int *ncolb)
 void matdet(double *a, int *n, double *result)
 {
     int info;
-    F77_CALL(dpotrf)("L", n, a, n, &info);
+    F77_CALL(dpotrf)("L", n, a, n, &info FCONE);
     if (info != 0)
         error("Cholesky decomposition failed");
     int in = n[0];
